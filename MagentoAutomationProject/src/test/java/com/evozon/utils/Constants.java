198e250ebb0511ecc9dd8b7a077c70fc33972442
Andrei Șaramet <andreisaramet21@gmail.com>
2023-08-07 14:06:01 +0300

    serenity magento automation project initial commit

--- MagentoAutomationProject/src/test/java/com/evozon/utils/Constants.java
+++ MagentoAutomationProject/src/test/java/com/evozon/utils/Constants.java
@@ -1,5 +1,5 @@
 package com.evozon.utils;
 
 public class Constants {
-    public static final String BASE_URL = "http://qa1magento.dev.evozon.com/";
+    public static final String BASE_URL = "http://qa3magento.dev.evozon.com/";
 }
