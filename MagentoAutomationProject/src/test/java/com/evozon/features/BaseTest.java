7b6f76a334e71d1082cc3ba5487b16d3dc1a8782
AlexandraArdelean <ale.xandra91@yahoo.com>
2023-08-07 14:47:44 +0300

    added login test

--- MagentoAutomationProject/src/test/java/com/evozon/features/BaseTest.java
+++ MagentoAutomationProject/src/test/java/com/evozon/features/BaseTest.java
@@ -1,8 +1,10 @@
 package com.evozon.features;
 
+import com.evozon.steps.LoginSteps;
 import com.evozon.utils.Constants;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
@@ -12,6 +14,9 @@ public class BaseTest {
 
     @Managed(uniqueSession = true)
     protected WebDriver driver;
+
+    @Steps
+    protected LoginSteps loginSteps;
 
     @Before
     public void init() {
--- MagentoAutomationProject/src/test/java/com/evozon/steps/BaseSteps.java
+++ MagentoAutomationProject/src/test/java/com/evozon/steps/BaseSteps.java
@@ -1,8 +1,12 @@
 package com.evozon.steps;
 
+import com.evozon.pages.AccountPage;
 import com.evozon.pages.HomePage;
+import com.evozon.pages.LoginPage;
 import net.thucydides.core.steps.ScenarioSteps;
 
 public class BaseSteps extends ScenarioSteps {
     protected HomePage homePage;
+    protected LoginPage loginPage;
+    protected AccountPage accountPage;
 }
