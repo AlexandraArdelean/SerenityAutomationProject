3e50a56630a3b5a45a47e561895830a3d72c7d00
AlexandraArdelean <ale.xandra91@yahoo.com>
2023-08-07 17:14:41 +0300

    changed username and password in Constants

--- MagentoAutomationProject/src/test/java/com/evozon/utils/Constants.java
+++ MagentoAutomationProject/src/test/java/com/evozon/utils/Constants.java
@@ -2,6 +2,7 @@ package com.evozon.utils;
 
 public class Constants {
     public static final String BASE_URL = "http://qa3magento.dev.evozon.com/";
-    public static final String USER_EMAIL = "john@email.com";
-    public static final String USER_PASSWORD = "password";
+    public static final String USER_EMAIL = "test@gmail.com";
+    public static final String USER_PASSWORD = "123456";
+    public static final String USER_NAME = "Test T Test";
 }
