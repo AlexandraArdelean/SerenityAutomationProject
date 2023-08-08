d04a9317063b86099640fb00abad15b44596bc21
AlexandraArdelean <ale.xandra91@yahoo.com>
2023-08-08 11:35:06 +0300

    added wishlist test

--- MagentoAutomationProject/src/test/java/com/evozon/pages/WishlistPage.java
+++ MagentoAutomationProject/src/test/java/com/evozon/pages/WishlistPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 public class WishlistPage extends BasePage {
     @FindBy(css = ".success-msg")
     private WebElementFacade addToWishlistSuccessMsg;
-    
+
 
 
 
